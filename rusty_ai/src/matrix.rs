43d43cada6269a7a5cb87c9cff58117e2c6ef4e5
Qwox <qwox@qwox.com>
2023-04-08 01:34:15 +0200

    impl Ring trait for all number types

--- rusty_ai/src/matrix.rs
+++ rusty_ai/src/matrix.rs
@@ -1,7 +1,25 @@
 use crate::util::macros::impl_getter;
 use crate::util::{dot_product, SetLength};
 use rand::Rng;
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
+use std::ops::{Add, Mul};
+
+pub trait Ring: Sized + Add<Self, Output = Self> + Mul<Self, Output = Self> {
+    const ZERO: Self;
+    const ONE: Self;
+}
+
+macro_rules! impl_ring {
+    ( $( $type:ty )+ : $zero:literal $one:literal ) => { $(
+        impl Ring for $type {
+            const ZERO: Self = $zero;
+            const ONE: Self = $one;
+        }
+    )+ };
+}
+impl_ring! { i8 i16 i32 i64 i128: 0 1 }
+impl_ring! { u8 u16 u32 u64 u128: 0 1 }
+impl_ring! { f32 f64: 0.0 1.0 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Matrix<T> {
@@ -13,7 +31,27 @@ pub struct Matrix<T> {
 impl_getter! { Matrix<T>:
     get_width -> width: usize,
     get_height -> height: usize,
-    get_elements -> elements: Vec<Vec<T>>
+    get_elements -> elements: Vec<Vec<T>>,
+}
+
+impl<T: Ring + Clone> Matrix<T> {
+    pub fn with_zero(width: usize, height: usize) -> Matrix<T> {
+        Matrix {
+            width,
+            height,
+            elements: vec![vec![T::ZERO; width]; height],
+        }
+    }
+
+    /// Creates the `n` by `n` identity Matrix.
+    pub fn identity(n: usize) -> Matrix<T> {
+        (0..n)
+            .into_iter()
+            .fold(Matrix::with_zero(n, n), |mut acc, i| {
+                acc.elements[i][i] = T::ONE;
+                acc
+            })
+    }
 }
 
 impl<T> Matrix<T> {
@@ -28,6 +66,8 @@ impl<T> Matrix<T> {
 }
 
 impl<T: Clone> Matrix<T> {
+    /// Uses first row for matrix width. All other rows are lengthed with `default` or shortend to
+    /// fit dimensions.
     pub fn from_rows(rows: Vec<Vec<T>>, default: T) -> Matrix<T> {
         let width = rows.get(0).map(Vec::len).unwrap_or(0);
         let height = rows.len();
@@ -94,10 +134,7 @@ macro_rules! impl_mul {
 
 impl_mul! { Matrix<T> &Matrix<T> }
 
-impl<T> std::fmt::Display for Matrix<T>
-where
-    T: std::fmt::Display,
-{
+impl<T: Display> Display for Matrix<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let column_widths = self.elements.iter().fold(vec![0; self.width], |acc, row| {
             row.iter()
@@ -105,8 +142,6 @@ where
                 .map(|(e, max)| std::cmp::max(max, e.to_string().len()))
                 .collect()
         });
-
-        // println!("{:?}", column_widths);
 
         let full_padding =
             " ".repeat(column_widths.len() + column_widths.iter().sum::<usize>() + 1);
@@ -127,5 +162,23 @@ where
                 .collect::<Vec<_>>()
                 .join("\n"),
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Matrix;
+
+    #[test]
+    fn identity() {
+        let res: Matrix<f64> = Matrix::identity(3);
+        assert_eq!(
+            res.elements,
+            vec![
+                vec![1.0, 0.0, 0.0],
+                vec![0.0, 1.0, 0.0],
+                vec![0.0, 0.0, 1.0]
+            ]
+        );
     }
 }
