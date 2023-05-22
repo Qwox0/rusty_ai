5e94c3a95aaa66e427f7312986ca8f444e137d99
Qwox <qwox@qwox.com>
2023-05-22 16:52:35 +0200

    replace bias markers with enum

--- rusty_ai/src/layer/builder.rs
+++ rusty_ai/src/layer/builder.rs
@@ -29,65 +29,60 @@ impl BuildWeights for WeightsInitialized {
     }
 }
 
-// Bias Markers
-pub struct OnePerNeuron(usize);
-pub struct BiasInitialized(LayerBias);
+#[derive(Debug, Clone)]
+enum BiasMarker {
+    OnePerNeuron { neurons: usize },
+    Initialized(LayerBias),
+}
 
-pub trait BuildBias {
-    fn build_bias(self) -> LayerBias;
-}
-impl BuildBias for OnePerNeuron {
+impl BiasMarker {
     fn build_bias(self) -> LayerBias {
-        let vec = rand::thread_rng()
-            .sample_iter(Uniform::from(0.0..1.0))
-            .take(self.0)
-            .collect();
-        LayerBias::OnePerNeuron(vec)
-    }
-}
-impl BuildBias for BiasInitialized {
-    fn build_bias(self) -> LayerBias {
-        self.0
+        match self {
+            BiasMarker::OnePerNeuron { neurons } => LayerBias::OnePerNeuron(
+                rand::thread_rng()
+                    .sample_iter(Uniform::from(0.0..1.0))
+                    .take(neurons)
+                    .collect(),
+            ),
+            BiasMarker::Initialized(bias) => bias,
+        }
     }
 }
 
 const DEFAULT_ACTIVATION_FN: ActivationFn = ActivationFn::default_relu();
 
 #[derive(Debug, Clone)]
-pub struct LayerBuilder<W, B> {
+pub struct LayerBuilder<W> {
     weights: W,
-    bias: B,
+    bias: BiasMarker,
     activation_function: Option<ActivationFn>,
 }
 
-impl LayerBuilder<Neurons, OnePerNeuron> {
-    pub fn new(neurons: usize) -> LayerBuilder<Neurons, OnePerNeuron> {
+impl LayerBuilder<Neurons> {
+    pub fn new(neurons: usize) -> LayerBuilder<Neurons> {
         LayerBuilder {
             weights: Neurons(neurons),
-            bias: OnePerNeuron(neurons),
+            bias: BiasMarker::OnePerNeuron { neurons },
             activation_function: None,
         }
     }
 }
 
-impl<B> LayerBuilder<Neurons, B> {
-    pub fn inputs(self, inputs: usize) -> LayerBuilder<LayerDimensions, B> {
+impl LayerBuilder<Neurons> {
+    pub fn inputs(self, inputs: usize) -> LayerBuilder<LayerDimensions> {
         let Neurons(neurons) = self.weights;
         let weights = LayerDimensions { inputs, neurons };
         LayerBuilder { weights, ..self }
     }
 
-    pub fn weights_checked(self, weights: Matrix<f64>) -> LayerBuilder<WeightsInitialized, B> {
+    pub fn weights_checked(self, weights: Matrix<f64>) -> LayerBuilder<WeightsInitialized> {
         assert_eq!(self.weights.0, weights.get_height());
         self.weights_unchecked(weights)
     }
 }
 
-impl<B> LayerBuilder<LayerDimensions, B> {
-    pub fn with_inputs(
-        inputs: usize,
-        neurons: usize,
-    ) -> LayerBuilder<LayerDimensions, OnePerNeuron> {
+impl LayerBuilder<LayerDimensions> {
+    pub fn with_inputs(inputs: usize, neurons: usize) -> LayerBuilder<LayerDimensions> {
         LayerBuilder::new(neurons).inputs(inputs)
     }
 
@@ -98,28 +93,28 @@ impl<B> LayerBuilder<LayerDimensions, B> {
     /// initializes `weights` to a specific value.
     /// # Panics
     /// This panics if the dimensions of `weights` don't match previously set layer dimensions.
-    pub fn weights_checked(self, weights: Matrix<f64>) -> LayerBuilder<WeightsInitialized, B> {
+    pub fn weights_checked(self, weights: Matrix<f64>) -> LayerBuilder<WeightsInitialized> {
         assert_eq!(self.get_dimensions(), weights.get_dimensions());
         self.weights_unchecked(weights)
     }
 }
 
-impl LayerBuilder<WeightsInitialized, OnePerNeuron> {
-    pub fn with_weights(weights: Matrix<f64>) -> LayerBuilder<WeightsInitialized, OnePerNeuron> {
+impl LayerBuilder<WeightsInitialized> {
+    pub fn with_weights(weights: Matrix<f64>) -> LayerBuilder<WeightsInitialized> {
         LayerBuilder::new(0).weights_unchecked(weights)
     }
 }
 
-impl<W, B> LayerBuilder<W, B> {
+impl<W> LayerBuilder<W> {
     /// initializes `weights` to a specific value. This ignores any previously set layer dimensions.
-    pub fn weights_unchecked(self, weights: Matrix<f64>) -> LayerBuilder<WeightsInitialized, B> {
+    pub fn weights_unchecked(self, weights: Matrix<f64>) -> LayerBuilder<WeightsInitialized> {
         let weights = WeightsInitialized(weights);
         LayerBuilder { weights, ..self }
     }
 
-    pub fn bias(self, bias: LayerBias) -> LayerBuilder<W, BiasInitialized> {
-        let bias = BiasInitialized(bias);
-        LayerBuilder { bias, ..self }
+    pub fn bias(mut self, bias: LayerBias) -> LayerBuilder<W> {
+        self.bias = BiasMarker::Initialized(bias);
+        self
     }
 
     pub fn activation_function(mut self, act_fn: ActivationFn) -> Self {
@@ -128,7 +123,7 @@ impl<W, B> LayerBuilder<W, B> {
     }
 }
 
-impl<W: BuildWeights, B: BuildBias> LayerBuilder<W, B> {
+impl<W: BuildWeights> LayerBuilder<W> {
     pub fn build(self) -> Layer {
         Layer {
             weights: self.weights.build_weights(),
@@ -138,13 +133,13 @@ impl<W: BuildWeights, B: BuildBias> LayerBuilder<W, B> {
     }
 }
 
-impl<B: BuildBias> LayerOrLayerBuilder for LayerBuilder<Neurons, B> {
+impl LayerOrLayerBuilder for LayerBuilder<Neurons> {
     fn as_layer_with_inputs(self, inputs: usize) -> Layer {
         self.inputs(inputs).build()
     }
 }
 
-impl<W: BuildWeights, B: BuildBias> LayerOrLayerBuilder for LayerBuilder<W, B> {
+impl<W: BuildWeights> LayerOrLayerBuilder for LayerBuilder<W> {
     fn as_layer_with_inputs(self, inputs: usize) -> Layer {
         let layer = self.build();
         assert_eq!(inputs, layer.get_input_count());
